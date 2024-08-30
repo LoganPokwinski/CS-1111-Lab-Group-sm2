1be329fd76466acb8acc342b67a555b0c25bdd89
alecnorder <acnorder@mtu.edu>
2024-08-30 09:06:11 -0400

    Add files via upload

---
+++ acnorder.c
@@ -0,0 +1,17 @@
+/*
+ * CS1111 lab1
+ *
+ * @author Alec Norder
+ * @email acnorder@mtu.edu
+ * @date 08-30-2024
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, Alec Norder! \n");
+	return 0;
+}
+
